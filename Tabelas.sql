90893acc350b4861f90a9bd5a1768765f68e4105
ivanor-upquery <ivanor@uquery.com>
2024-08-26 09:04:15 -0300

    criado plb

--- Tabelas.sql
+++ Tabelas.sql
@@ -8,6 +8,7 @@ Baseadas no agente atual
 ctb_acoes      - (adicionado comando e comando_limpar)  - CREATE unique INDEX ctb_acoes_IDX01 ON ctb_acoes (id_acao); 
 CTB_ACOES_exec - (adicionado  run_acao_id, erro, comando_limpar)  - criado indexes 
 TMP_DOCS       - CREATE INDEX tmp_docs_IDX01 ON tmp_docs (id_cliente, id_acao); 
+ctb_cliente_usuario - clientes que o usuário tem acesso 
 
 - Não alterar porque também é usado pelo agente 
 CTB_UPDATE    -- Alteração de URL ??
